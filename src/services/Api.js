d80cfd41643a2242acd747244b546ec87ac091ab
root <root@vps526554.ovh.net>
2018-03-19 12:40:42 +0100

    hotfix

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     // baseURL: 'http://192.168.1.208:8081'
-    baseURL: 'http://localhost:8081'
+    // baseURL: 'http://localhost:8081'
+    baseURL: 'http://vps526554.ovh.net'
   })
 }
